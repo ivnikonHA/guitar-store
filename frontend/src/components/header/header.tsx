18b9c237f0d4a984bcb2f5068cfc56407e5abf30
ivnikonHA <ivnikon@mail.ru>
2024-09-18 14:58:07 +0300

    Добавляет отображение имени авторизованного пользователя в шапке

--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -1,7 +1,12 @@
+import { useAppSelector } from '../../hooks/use-app-selector';
+import { getIsAuthorized, getUserData } from '../../store/user/user-selectors';
 import Logo from '../logo/logo';
 import MainNavigation from '../main-navigation/main-navigation';
 
 function Header(): JSX.Element {
+  const isAuthorized = useAppSelector(getIsAuthorized);
+  const userData = useAppSelector(getUserData);
+
   return (
     <header className="header" id="header">
       <div className="container">
@@ -11,7 +16,7 @@ function Header(): JSX.Element {
             <MainNavigation />
           </nav>
           <div className="header__container">
-            <span className="header__user-name">Имя</span>
+            <span className="header__user-name">{isAuthorized ? userData.name : ''}</span>
             <a className="header__link" href="login.html" aria-label="Перейти в личный кабинет">
               <svg className="header__link-icon" width="12" height="14" aria-hidden="true">
                 <use xlinkHref="#icon-account"></use>
